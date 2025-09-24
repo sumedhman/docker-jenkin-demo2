5033d29451ef10979be8c4124535aacb95da9bf2
sumedhman <sumedh.manwatkar@gmail.com>
2025-09-24 16:42:11 +0530

    my first commit

---
+++ src/main/java/com/gla/jenkin/DockerJenkinDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gla.jenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gla/jenkin/DockerJenkinDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gla.jenkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
